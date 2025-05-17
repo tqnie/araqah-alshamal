d8d8e8c9562dfa63a21b4bb2f52328732235f5f6
tqnie <tigertheblack2000@gmail.com>
2025-05-17 16:42:57 +0300

    سس

--- resources/views/building_plans/index.blade.php
+++ resources/views/building_plans/index.blade.php
@@ -11,7 +11,15 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     
 <style>
-   
+    :root {
+        --primary-color: #3490dc;
+        --secondary-color: #38c172;
+        --danger-color: #e3342f;
+        --warning-color: #ffed4a;
+        --light-color: #f8f9fa;
+        --dark-color: #343a40;
+        --border-color: #dee2e6;
+    }
 
     .building-layout-container {
         position: relative;
